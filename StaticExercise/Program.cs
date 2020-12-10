6a697172b71f013b10f68aef8bfc56f2a1ae1a1d
Amir Gaffar <AmirG7@yahoo.com>
2020-12-10 11:57:16 -0500

    Completed Exercise on TempConverter

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var celsius = TempConverter.FarenheitToCelsius(68);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+
+            Console.WriteLine($"After conversion:");
+            Console.WriteLine($"celsius {celsius}");
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+
+        }
+
+       
+
+    }
+
+
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FarenheitToCelsius(double farenheit)
+        {
+           var result = (farenheit - 32) / 1.8;
+
+
+            return result;
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9) / 5 + 32;
+        }
+    }
+}
